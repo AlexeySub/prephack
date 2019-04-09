79b8149f448091aa95c2dc1a745d3c9020cb7463
AlexeySub <mr.317676@gmail.com>
2019-04-09 11:50:28 +0300

    1

--- hack/settings.py
+++ hack/settings.py
@@ -91,10 +91,6 @@ WSGI_APPLICATION = 'hack.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'dir': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'ddj2fcnrq9ep4p',
